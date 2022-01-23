1711f7b6eceaa2e1bf3865d6615ae1165499d596
praveenk1029 <43780238+praveenk1029@users.noreply.github.com>
2022-01-23 16:33:04 -0500

    Added getNames API.

--- src/main/java/com/practice/testapp/controller/TestController.java
+++ src/main/java/com/practice/testapp/controller/TestController.java
@@ -4,7 +4,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @GetMapping("/getInfo")
     public String getInfo(){
         return "getInfo invoked...";
+    }
+    
+    @GetMapping("/getNames")
+    public List<String> getNames(){
+        return Arrays.asList("Sam", "Nicole", "Rachel");
     }
 }
